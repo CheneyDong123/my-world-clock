9e9267b76cb4f1146cac619a436b1f5641fd1998
dong <CheneyDong123@163.com>
2022-07-23 12:51:14 +0800

    feat: setup componets

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import Hello from "@/components/Hello.vue";
 </script>
 
 <template>
-  <div>hello</div>
+  <Hello name="chen"/>
 </template>
 
 <style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  name: String
+}>()
+</script>
+
+<template>
+  <div>hello, {{ name }}</div>
+</template>
+
+<style>
+</style>
